8e4cf8936ce52c2906227db86b5bd8d1add04c71
miwanoff <marina.iva.noffa@gmail.com>
2025-04-27 10:51:47 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function openFord() {
+  window.open("ford.html", "ford", "width=400,height=300");
+}
+function openMazda() {
+  window.open("mazda.html", "mazda", "width=400,height=300");
+}
+function openVolvo() {
+  window.open("volvo.html", "volvo", "width=400,height=300");
+}
+function closePict() {
+  window.close();
+}
