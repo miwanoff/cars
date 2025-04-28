d6ba36784ec0ad0b35499603cab506839ffe8a5f
miwanoff <marina.iva.noffa@gmail.com>
2025-04-28 21:22:02 +0300

    Add openCar

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function openFord() {
-  window.open("ford.html", "ford", "width=400,height=300");
+// function openFord() {
+//   window.open("ford.html", "ford", "width=400,height=300");
+// }
+// function openMazda() {
+//   window.open("mazda.html", "mazda", "width=400,height=300");
+// }
+// function openVolvo() {
+//   window.open("volvo.html", "volvo", "width=400,height=300");
+// }
+
+function openCar(car) {
+  window.open(car + ".html", car, "width=400,height=300");
 }
-function openMazda() {
-  window.open("mazda.html", "mazda", "width=400,height=300");
-}
-function openVolvo() {
-  window.open("volvo.html", "volvo", "width=400,height=300");
-}
+
 function closePict() {
   window.close();
 }
